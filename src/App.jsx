623d5e2fff17bacce4383d3418855b78382beabd
Val-bejah <116029510+Val-bejah@users.noreply.github.com>
2024-04-04 15:41:44 -0500

    lista de prductos diarios

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
-import { Routes, Route, } from "react-router";
+import { Routes, Route } from "react-router";
 import { Suspense } from "react";
 import { Home } from "./pages/Home/Home";
 import { Header } from "./pages/sections/Header/Header";
 import { Login } from "./pages/Auth/Login/Login";
 import { Singup } from "./pages/Auth/Singup/Singup";
+import { Diary } from "./pages/Diary/Diary";
 
 function App() {
   return (
     <Suspense fallback={<div>loading...</div>}>
       <Routes>
         <Route path="/" element={<Header />}>
-          <Route index element={<Home/>}/>
-          <Route path="/login" element={<Login/>}/>
-          <Route path="/singup" element={<Singup/>}/>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/singup" element={<Singup />} />
+          <Route path="/diary" element={<Diary />} />
         </Route>
       </Routes>
     </Suspense>
