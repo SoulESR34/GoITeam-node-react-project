8e5ab90f1ceed01b8b1d32a6b832268b9e27472e
Saul Esteban Santana Romero <siesaul1111@gmail.com>
2024-04-05 10:10:58 -0500

    fixed: github pages url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://soulesr34.github.io/GoITeam-node-react-project/"
+  base: "https://github.com/SoulESR34/GoITeam-node-react-project"
 })
